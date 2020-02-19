bfe178d2b676ad5cd9d0d4b3459c7e8253b2ed4b
Eti <nehu@Pixel>
2020-02-19 23:36:03 +0100

    Added support for an optional build command

--- src/deployer.ts
+++ src/deployer.ts
@@ -1,9 +1,22 @@
 import simplegit from "simple-git";
 import {Git} from "simple-git/src/index";
+import { execSync } from "child_process";
 
 export default class Deployer
 {
-	public static deploy() : Git
+	public static deploy() : void
+	{
+		Deployer.updateRepository();
+
+		if (process.env.build_command)
+		{
+			console.log("Performing build step");
+			console.log(execSync(process.env.build_command).toString());
+			console.log("Build step finished");
+		}
+	}
+
+	private static updateRepository() : Git
 	{
 		if (!process.env.destination)
 			throw new Error("The \"destination\" environment variable must be set to point to the local repository");
